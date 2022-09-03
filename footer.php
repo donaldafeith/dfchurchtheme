0da6a59c77e25f04a9a437aa6a2d23d7f5628afb
Donalda Feith <donaldafeith@gmail.com>
2022-09-03 00:22:35 -0400

    Theme Commit V 0.1

--- footer.php
+++ footer.php
@@ -10,25 +10,53 @@
  */
 
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dfchurchtheme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'dfchurchtheme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'dfchurchtheme' ), 'dfchurchtheme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<footer id="colophon" class="site-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="widget">
+					<h3 class="widget-title">Our address</h3>
+					<?php if ( is_active_sidebar( 'footer1' ) ) : ?>
+						<div id="footer1" class="footer1 widget-area" role="complementary">
+							<?php dynamic_sidebar( 'footer1' ); ?>
+						</div><!-- #Sermons -->
+						<?php endif; ?>	
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="widget">
+						<h3 class="widget-title">Topics from last meeting</h3>
+						<?php if ( is_active_sidebar( 'footer2' ) ) : ?>
+							<div id="footer2" class="footer2 widget-area" role="complementary">
+								<?php dynamic_sidebar( 'footer2' ); ?>
+							</div><!-- #Sermons -->
+							<?php endif; ?>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="widget">
+							<h3 class="widget-title">Contact form</h3>
+							<?php if ( is_active_sidebar( 'footer2' ) ) : ?>
+								<div id="footer3" class="footer3 widget-area" role="complementary">
+									<?php dynamic_sidebar( 'footer2' ); ?>
+								</div><!-- #Sermons -->
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+					<!-- .row -->
+					<p class="colophon"><div class="site-info">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dfchurchtheme' ) ); ?>">
+						<?php /* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'dfchurchtheme' ), 'WordPress' );
+						?>
+						</a>
+						<span class="sep"> | </span>
+						<?php /* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'dfchurchtheme' ), 'dfchurchtheme', '<a href="https://github.com/donaldafeith">Donalda</a>' ); ?></p>
+						</div><!-- .container -->
+					</footer> <!-- .site-footer -->
+				</div>
+				<?php wp_footer(); ?>
+			</body>
+			</html>
